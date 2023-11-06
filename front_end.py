3d7964583743dc28f03fde49ae946ef64dc8d877
rakibulhaque9954 <rakibulhaque9954@gmail.com>
2023-11-06 20:05:50 +0900

    default image add

--- front_end.py
+++ front_end.py
@@ -52,6 +52,37 @@ def index():
             return render_template('index.html', error='Invalid file type.')
     return render_template('index.html')
 
+@app.route('/test_image')
+def test_image():
+    default_image_filename = 'test_img.jpeg'  # Replace with your default image file name
+    original_file_path = os.path.join(app.config['UPLOAD_FOLDER'], default_image_filename)
+
+    # Posting to the SRGAN model service
+    response = requests.post('https://srgan-model-ver-1-znzp2767aq-an.a.run.app',
+                             files={'file': open(original_file_path, 'rb')})
+
+    try:
+        data = response.json()
+    except requests.exceptions.JSONDecodeError as json_err:
+        print(f"Failed to decode JSON from response: {json_err}")
+        print("Response content:", response.text)
+        return render_template('index.html', error='Could not decode the response.')
+
+    # Convert the list back to a NumPy array and then to an image
+    gen_img_list = data['Prediction Tensor']
+    gen_img_np = np.array(gen_img_list)
+    gen_img = Image.fromarray((gen_img_np[0] * 255).astype(np.uint8))
+
+    # Save the generated image
+    enhanced_filename = f'enhanced_{default_image_filename}'
+    enhanced_file_path = os.path.join(app.config['UPLOAD_FOLDER'], enhanced_filename)
+    gen_img.save(enhanced_file_path)
+
+    return render_template('result.html',
+                           original_image=url_for('static', filename=f'uploads/{default_image_filename}'),
+                           generated_image=url_for('static', filename=f'uploads/{enhanced_filename}'))
+
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
